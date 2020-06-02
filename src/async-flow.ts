f48aca013a9c1f20d1eb43717a2553c70595f0d6
xuexin <tomyail1989@gmail.com>
2020-06-02 13:31:54 +0800

    fix: 修复并发结果不对

--- src/async-flow.ts
+++ src/async-flow.ts
@@ -1,16 +1,17 @@
-import _ from "lodash";
-import { combineLatest, concat, defer, ObservableInput } from "rxjs";
-import { map, mergeMap, reduce } from "rxjs/operators";
+import _ from 'lodash'
+import { combineLatest, concat, defer, ObservableInput, of } from 'rxjs'
+import { map, mergeMap, reduce } from 'rxjs/operators'
 
+//https://stackblitz.com/edit/jo3dq1-c3kppz?file=index.ts
 interface FlowConfig<T> {
-  name: string;
-  flow: () => ObservableInput<T>;
-  map: (result, context) => any;
+  name: string
+  flow: (context) => ObservableInput<T>
+  map: (result, context) => any
   children?:
-  | FlowConfig<T>[]
-  | {
-    [key: string]: FlowConfig<T>;
-  };
+    | FlowConfig<T>[]
+    | {
+        [key: string]: FlowConfig<T>
+      }
 }
 export const flow = <T>(
   context,
@@ -18,53 +19,47 @@ export const flow = <T>(
 ) => {
   if (_.isArray(flowConfigs)) {
     //@ts-ignore
-    return flowArray(context, flowConfigs);
+    return flowArray(context, flowConfigs)
   } else {
     //@ts-ignore
-    return flowMap(context, flowConfigs);
+    return flowMap(context, flowConfigs)
   }
-};
+}
 
 const flowArray = <T>(context, flows: FlowConfig<T>[]) => {
-  return concat(...flows.map(flow => flowOne(context, flow))).pipe(
-    reduce(() => context, context)
-  );
-};
+  return concat(...flows.map((flow) => flowOne(context, flow))).pipe(reduce(() => context, context))
+}
 
 const flowMap = <T>(context: any, flows: { [key: string]: FlowConfig<T> }) => {
   return combineLatest(
     ...Object.keys(flows)
-      .map(key => {
+      .map((key) => {
         if (flows[key].name !== key) {
-          throw new Error("flowConfig's key must save to name");
+          throw new Error("flowConfig's key must save to name")
         }
-        return key;
+        return key
       })
-      .map(key => {
-        const config = flows[key];
-        return flowOne(context, config);
+      .map((key) => {
+        const config = flows[key]
+        return flowOne(context, config)
       })
   ).pipe(
-    map(item => {
-      return item;
+    map((item) => {
+      return context
     })
-  );
-};
+  )
+}
 const flowOne = <T>(context, config: FlowConfig<T>) => {
-  return defer(config.flow).pipe(
-    mergeMap(result => {
-      context[config.name] = config.map(result, context);
+  return defer(() => config.flow(context)).pipe(
+    mergeMap((result) => {
+      context[config.name] = config.map(result, context)
       if (config.children) {
-        return flow(
-          context,
-          config.children
-        );
+        return flow(context, config.children)
       }
-      return [];
+      return of(context[config.name])
     }),
-    map(result => {
-      return context;
+    map((result) => {
+      return result
     })
-  );
-};
-
+  )
+}
